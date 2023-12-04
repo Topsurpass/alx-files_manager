88d78205fb1df809811081102b9d5ca9e8450a07
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-12-04 01:27:06 +0100

    added the route for authentication

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import AppController from "../controllers/AppController";
 import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
 
 /**
  * This contains all the routes of my application.
@@ -11,5 +12,8 @@ function allAppRoutes(server) {
 	server.get("/status", AppController.getStatus);
     server.get("/stats", AppController.getStats);
     server.post("/users", UsersController.postNew);
+    server.get("/connect", AuthController.getConnect);
+    server.get("/disconnect", AuthController.getDisconnect);
+    server.get("/users/me", UsersController.getMe);
 }
 export default allAppRoutes;
